c477c5aa8277507ca32b9ad844d71742aaf229a8
guptaprkhr <gupta.prkhr@gmail.com>
2017-03-27 12:05:10 +0200

    Adding the tweet and wiki tokenizer files
    Run command 
    python2 tweet(wiki)Tokenize.py Filename > outputFile to get the tokenized sentences in the outputFile

---
+++ tweetTokenize.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python2
+
+import sys
+from nltk.tokenize import TweetTokenizer
+import re
+
+def tokenize(tknzr, sentence, to_lower=True):
+    """Arguments:
+        - tknzr: a tokenizer implementing the NLTK tokenizer interface
+        - sentence: a string to be tokenized
+        - to_lower: lowercasing or not
+    """
+    sentence = sentence.strip()
+    sentence = ' '.join([format_token(x) for x in tknzr.tokenize(sentence)])
+    if to_lower:
+        sentence = sentence.lower()
+    sentence = re.sub('((www\.[^\s]+)|(https?://[^\s]+)|(http?://[^\s]+))','<url>',sentence) #replace urls by <url>
+    sentence = re.sub('(\@[^\s]+)','<user>',sentence) #replace @user268 by <user>
+    filter(lambda word: ' ' not in word, sentence)
+    return sentence
+
+def format_token(token):
+    """"""
+    if token == '-LRB-':
+        token = '('
+    elif token == '-RRB-':
+        token = ')'
+    elif token == '-RSB-':
+        token = ']'
+    elif token == '-LSB-':
+        token = '['
+    elif token == '-LCB-':
+        token = '{'
+    elif token == '-RCB-':
+        token = '}'
+    return token
+
+def tokenize_sentences(tknzr, sentences, to_lower=True):
+    """Arguments:
+        - tknzr: a tokenizer implementing the NLTK tokenizer interface
+        - sentences: a list of sentences
+        - to_lower: lowercasing or not
+    """
+    return [tokenize(tknzr, s, to_lower) for s in sentences]
+
+
+
+
+
+fileName = sys.argv[1]
+
+from nltk.tokenize import TweetTokenizer
+tknzr = TweetTokenizer()
+
+
+sentences = []
+with open(fileName, 'r') as fileinput:
+   for line in fileinput:
+       sentences.append(line)
+
+tknzr = TweetTokenizer()
+tokenized_sentences_NLTK_tweets = tokenize_sentences(tknzr, sentences)
+
+for sentence in tokenized_sentences_NLTK_tweets:
+    print (sentence)
---
+++ wikiTokenize.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python2
+
+import sys
+from nltk.tokenize import TweetTokenizer
+import re
+
+def tokenize(tknzr, sentence, to_lower=True):
+    """Arguments:
+        - tknzr: a tokenizer implementing the NLTK tokenizer interface
+        - sentence: a string to be tokenized
+        - to_lower: lowercasing or not
+    """
+    sentence = sentence.strip()
+    sentence = ' '.join([format_token(x) for x in tknzr.tokenize(sentence)])
+    if to_lower:
+        sentence = sentence.lower()
+    sentence = re.sub('((www\.[^\s]+)|(https?://[^\s]+)|(http?://[^\s]+))','<url>',sentence) #replace urls by <url>
+    sentence = re.sub('(\@[^\s]+)','<user>',sentence) #replace @user268 by <user>
+    filter(lambda word: ' ' not in word, sentence)
+    return sentence
+
+def format_token(token):
+    """"""
+    if token == '-LRB-':
+        token = '('
+    elif token == '-RRB-':
+        token = ')'
+    elif token == '-RSB-':
+        token = ']'
+    elif token == '-LSB-':
+        token = '['
+    elif token == '-LCB-':
+        token = '{'
+    elif token == '-RCB-':
+        token = '}'
+    return token
+
+def tokenize_sentences(tknzr, sentences, to_lower=True):
+    """Arguments:
+        - tknzr: a tokenizer implementing the NLTK tokenizer interface
+        - sentences: a list of sentences
+        - to_lower: lowercasing or not
+    """
+    return [tokenize(tknzr, s, to_lower) for s in sentences]
+
+
+
+
+
+fileName = sys.argv[1]
+
+from nltk.tokenize import TweetTokenizer
+tknzr = TweetTokenizer()
+
+
+sentences = []
+with open(fileName, 'r') as fileinput:
+   for line in fileinput:
+       sentences.append(line)
+
+tknzr = StanfordTokenizer(SNLP_TAGGER_JAR, encoding='utf-8')
+s = ' <delimiter> '.join(sentences)
+tokenized_sentences_SNLP = tokenize_sentences(tknzr, [s])
+tokenized_sentences_SNLP = tokenized_sentences_SNLP[0].split(' <delimiter> ')
+
+for sentence in tokenized_sentences_SNLP:
+    print (sentence)
