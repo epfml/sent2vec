be0f35df80c30f89956df9a1cb5aed23b457352f
hanabi1224 <harlowmoo@gmail.com>
2019-09-27 01:09:39 +0800

    build shared library

---
+++ src/sent2vec.cc
@@ -0,0 +1,38 @@
+﻿
+#include "fasttext.h"
+
+using namespace fasttext;
+
+extern "C" void* loadModel(char* path)
+{
+    FastText* fastText = new FastText();
+    (*fastText).loadModel(std::string(path));
+    return fastText;
+}
+
+extern "C" void freeModel(void* modelPtr)
+{
+    FastText* model = (FastText*)modelPtr;
+    free(model);
+}
+
+extern "C" int getDimension(void* modelPtr)
+{
+    FastText* model = (FastText*)modelPtr;
+    return (*model).getDimension();
+}
+
+extern "C" void getSentenceVector(
+    void* modelPtr,
+    char* input,
+    float* svec
+)
+{
+    FastText* model = (FastText*)modelPtr;
+    int dim = (*model).getDimension();
+    std::vector<std::string> strList(1);
+    strList[0] = std::string(input);
+    std::vector<float> vector(dim);
+    (*model).textVectors(strList, 1, vector);
+    memcpy(svec, &vector[0], vector.size() * sizeof(float));
+}
